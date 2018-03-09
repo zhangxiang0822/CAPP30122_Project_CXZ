dbe0697927e732d41be9c05b663a84178e3dace0
Xiang <zhangxiang0822@gmail.com>
2018-03-08 19:59:05 -0600

    14th

--- code/analysis/draw_distribution_basemap.py
+++ code/analysis/draw_distribution_basemap.py
@@ -265,11 +265,11 @@ if __name__ == "__main__":
     
     data= data.set_index("FIPS")
     
-    state_list = ["30", "31", "32", "33", "34", \
-                  "35", "36", "37", "38", "39", "40", "41", "42"]
+    state_list = ["30", "31", "33", "34", \
+                  "35", "36", "37", "38", "39"]
     
     for FIPS in FIPS_list:
-        if FIPS[0:2] in ["42"]:
+        if FIPS[0:2] in ["36"]:
             start = time.clock()   
             plot_county_location(FIPS, data)
             print(FIPS, time.clock() - start) 
